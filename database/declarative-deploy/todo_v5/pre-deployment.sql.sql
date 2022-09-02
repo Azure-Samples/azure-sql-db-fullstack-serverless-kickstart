1b1ce1667fb2ed8d1b121bd1b0c8e4c0900572f2
Davide Mauri <damauri@microsoft.com>
2022-09-02 15:48:27 -0700

    corrected typo

--- database/declarative-deploy/todo_v5/pre-deployment.sql.sql
+++ database/declarative-deploy/todo_v5/pre-deployment.sql.sql
@@ -1,5 +1,5 @@
 -- This file contains SQL statements that will be executed before the build script.
 
-if (object_id('dbo.todoso') is not null) begin
+if (object_id('dbo.todos') is not null) begin
     delete from dbo.todos
 end
