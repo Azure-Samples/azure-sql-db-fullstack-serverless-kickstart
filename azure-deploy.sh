6e3b9d1d4ccb0442cfeaf8950fb477ca725492a0
Davide Mauri <damauri@microsoft.com>
2022-09-09 15:00:50 -0700

    removing hard-coded value

--- azure-deploy.sh
+++ azure-deploy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 FILE=".env"
 if [[ -f $FILE ]]; then
 	echo "Loading from $FILE"
-	eval $(egrep "^[^#;]" .env | tr '\n' '\0' | xargs -0 -n1 | sed 's/^/export /')
+	eval $(egrep "^[^#;]" $FILE | tr '\n' '\0' | xargs -0 -n1 | sed 's/^/export /')
 else
 	cat << EOF > .env
 resourceGroup=""
