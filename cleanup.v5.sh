d9edd5786fff0cb9e498734a1658b65b35e7f340
Davide Mauri <damauri@microsoft.com>
2022-11-11 13:30:55 -0800

    cleanup script

---
+++ cleanup.v5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Preparing sample for v5.0..."
+
+# cleanup API binaries
+rm -rf ./api/bin
+rm -rf ./api/obj
+
+# cleanup CLIENT
+rm -rf ./client/node_modules
+rm -rf ./client/dist
+
+# cleanup DATABASE
+rm -rf ./database/deploy
+
+# cleanup TEST
+rm -rf ./test
+
+echo "Done"
