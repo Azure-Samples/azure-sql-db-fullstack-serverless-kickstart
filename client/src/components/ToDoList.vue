4604c7d04d542c623072b3ace881eec4a850a052
Davide Mauri <damauri@microsoft.com>
2022-11-11 16:15:21 -0800

    updated to vue3+vite

--- client/src/components/ToDoList.vue
+++ client/src/components/ToDoList.vue
@@ -37,16 +37,17 @@
 		</footer>
   </section> 
   <footer class="info">
-		<label id="login">[<a href=".auth/login/github">login</a>]</label>
-		<label id="logoff">[<a href=".auth/logout">logoff</a>]</label>
+    <label v-if="!userDetails" id="login">[<a href=".auth/login/github">login</a>]</label>
+		<label v-if="userDetails" id="logoff">[<a href=".auth/logout">logoff {{userDetails}}</a>]</label>    
 		<p>Double-click to edit a todo</p>
 		<p>Original <a href="https://github.com/vuejs/vuejs.org/tree/master/src/v2/examples/vue-20-todomvc">Vue.JS Sample</a> by <a href="http://evanyou.me">Evan You</a></p>		
 		<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>        
 	</footer> 
 </template>
 
-<script>
-var API = "/api/todo";
+<script lang="js">
+
+var API = "/api/ef/todo";
 var HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json' };		
 
 var filters = {
@@ -65,13 +66,14 @@ var filters = {
   }
 };
 
-export default {
+export default {  
   data() {
     return {
       todos: [],
       newTodo: "",
 			editedTodo: null,
-			visibility: "all"
+			visibility: "all",
+      userDetails: null
     };
   },
   
@@ -84,6 +86,15 @@ export default {
       this.visibility = "all";
     }
 
+    fetch('/.auth/me')
+    .then(res => {
+      return res.json()
+    })
+    .then(payload => {
+      const { clientPrincipal } = payload;
+			this.userDetails = clientPrincipal?.userDetails;				
+    });						
+
     fetch(API, { 
       headers: HEADERS, 
       method: "GET"      
@@ -91,7 +102,7 @@ export default {
       return res.json();
     }).then(res => {				
       this.todos = res == null ? [] : res;
-    })    
+    });
   },
   
   computed: {
@@ -179,7 +190,7 @@ export default {
         return term + 's';
       else
         return term;
-    }
+    }    
   },
 
   directives: {
