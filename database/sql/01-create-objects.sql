9f7a47575557a2bd9967b45fefaf1734d877c561
Davide Mauri <damauri@microsoft.com>
2024-01-16 10:59:50 -0800

    updated script

--- database/sql/01-create-objects.sql
+++ database/sql/01-create-objects.sql
@@ -1,10 +1,39 @@
+if (serverproperty('Edition') = 'SQL Azure') begin
+
+    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'webapp')
+    begin 
+        create user [webapp] with password = 'Super_Str0ng*P4ZZword!'        
+    end
+
+    alter role db_owner add member [webapp]
+    
+end else begin
+
+    if not exists (select * from sys.server_principals where [type] in ('E', 'S') and [name] = 'webapp')
+    begin 
+        create login [webapp] with password = 'Super_Str0ng*P4ZZword!'
+    end    
+
+    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'webapp')
+    begin
+       create user [webapp] from login [webapp]   
+    end
+
+    alter role db_owner add member [webapp]
+end
+go
+
 create schema [web] authorization [dbo];
 go
 
-create user [webapp] with password = 'Super_Str0ng*P4ZZword!'
+grant execute on schema::[web] to [webapp]
 go
 
-grant execute on schema::[web] to [webapp]
+/* needed for EF Core */
+grant select on [dbo].[todos] to [webapp]
+grant insert on [dbo].[todos] to [webapp]
+grant update on [dbo].[todos] to [webapp]
+grant delete on [dbo].[todos] to [webapp]
 go
 
 create sequence dbo.[global_sequence]
