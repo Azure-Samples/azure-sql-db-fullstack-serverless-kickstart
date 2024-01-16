f763872f48bcfbe52bf399f217655cc164676730
Davide Mauri <damauri@microsoft.com>
2024-01-16 12:29:38 -0800

    added app user creation script

--- database/sql/01-create-objects.sql
+++ database/sql/01-create-objects.sql
@@ -1,28 +1,3 @@
-if (serverproperty('Edition') = 'SQL Azure') begin
-
-    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'webapp')
-    begin 
-        create user [webapp] with password = 'Super_Str0ng*P4ZZword!'        
-    end
-
-    alter role db_owner add member [webapp]
-    
-end else begin
-
-    if not exists (select * from sys.server_principals where [type] in ('E', 'S') and [name] = 'webapp')
-    begin 
-        create login [webapp] with password = 'Super_Str0ng*P4ZZword!'
-    end    
-
-    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'webapp')
-    begin
-       create user [webapp] from login [webapp]   
-    end
-
-    alter role db_owner add member [webapp]
-end
-go
-
 create schema [web] authorization [dbo];
 go
 
